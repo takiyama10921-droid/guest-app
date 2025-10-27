7a1550f30b680dfb33dc0aa43202d4b5fa3637a9
tomoki <mufgs1087@outlook.jp>
2025-10-27 18:21:51 +0900

    メッセージ変更

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -4,7 +4,7 @@ export default function HomePage() {
   return (
     <div style={{ textAlign: "center", marginTop: "50px" }}>
       <h1>結婚式ゲスト用ページ</h1>
-      <p>招待状のQRコードを読み取ると、個別ページが開きます。</p>
+      <p>招待状のQRコードを読み取ると、個別ページが開きます!</p>
       <p>テストリンク:</p>
       <Link to="/guest/abc123">山田太郎のページへ</Link>
     </div>
