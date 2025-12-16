03a09365546f3214d93087ddf3b85120f64d9ae4
tomoki <mufgs1087@outlook.jp>
2025-12-16 21:00:03 +0900

    map変更

--- src/pages/VenueMapPage.tsx
+++ src/pages/VenueMapPage.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from "react";
 import Header from "../components/Header";
-import mapImage from "../assets/img_access01.jpg";
+import mapImage from "../assets/map.jpg";
 
 const VenueMapPage: React.FC = () => {
   useEffect(() => {
